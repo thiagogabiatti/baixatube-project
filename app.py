c41b5745c184ab293f99732705734f57e84c8e8a
Thiago Gabiatti <thiago.gabiatti@hotmail.com>
2024-09-05 16:47:57 -0300

    Att do site e pre deploy no heroku

--- app.py
+++ app.py
@@ -1,63 +1,67 @@
-from flask import Flask, render_template, request, send_file, after_this_request
+from flask import Flask, render_template, request, send_file, after_this_request, flash, redirect, url_for
 from pytubefix import YouTube
 import os
 import zipfile
+import io
+import re
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+def is_valid_youtube_url(url):
+    youtube_regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+')
+    return youtube_regex.match(url)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        links = request.form['links'] #return string with links from textarea
+        links = request.form['links']
         link_list = links.splitlines()
         format_type = request.form['format']
-        downloaded_files = [] # list to store paths from downloaded files
+        downloaded_files = []
+
+        invalid_links = [link for link in link_list if not is_valid_youtube_url(link)]
+        
+        if invalid_links:
+            flash(f'Link inválido: {", ".join(invalid_links)}')
+            return redirect(url_for('index'))  # Redireciona para evitar problemas ao recarregar a página
 
         for link in link_list:
-            yt = YouTube(link)
-            file_path = None
+            try:
+                yt = YouTube(link)
+                file_path = None
 
-            if format_type == 'mp4':
-                # Download video as MP4
-                video_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-                file_path = video_stream.download()
+                if format_type == 'mp4':
+                    video_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+                    file_path = video_stream.download()
 
-            elif format_type == 'mp3':
-                # Download audio as MP3
-                audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
-                file_path = audio_stream.download()
-                # Rename file to .mp3
-                base, ext = os.path.splitext(file_path)
-                new_file = base + '.mp3'
-                os.rename(file_path, new_file)
-                file_path = new_file
+                elif format_type == 'mp3':
+                    audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+                    file_path = audio_stream.download()
+                    base, ext = os.path.splitext(file_path)
+                    new_file = base + '.mp3'
+                    os.rename(file_path, new_file)
+                    file_path = new_file
 
-            if file_path:
-                downloaded_files.append(file_path)
+                if file_path:
+                    downloaded_files.append(file_path)
 
-        # zip all files
-        zip_filename = 'mediasdownload.zip'
-        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            except Exception as e:
+                flash(f'Erro ao baixar o link {link}: {e}')
+                return redirect(url_for('index'))
+
+        memory_file = io.BytesIO()
+        with zipfile.ZipFile(memory_file, 'w') as zipf:
             for file in downloaded_files:
                 zipf.write(file, os.path.basename(file))
 
-        @after_this_request
-        def remove_files(response):
-            try:
-                # remove individual files
-                for file in downloaded_files:
-                    if os.path.exists(file):
-                        os.remove(file)
-                # remove zip file
-                if os.path.exists(zip_filename): 
-                    os.remove(zip_filename)
-            except Exception as e:
-                print(f"Error removing file: {e}")
-            return response
+        memory_file.seek(0)
 
+        for file in downloaded_files:
+            if os.path.exists(file):
+                os.remove(file)
 
-        return send_file(zip_filename, as_attachment=True)
-
+        return send_file(memory_file, as_attachment=True, download_name='mediasdownload.zip')
 
     return render_template('index.html')
 
